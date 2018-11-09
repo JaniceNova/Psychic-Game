8023bbfbb58909a9e97c02941965dc4fdd98e214
Bryan Liu <bryanliu@macbook-pro.attlocal.net>
2018-11-09 03:51:05 -0800

    not connected yet

---
+++ assests/javascript/game.js
@@ -0,0 +1,5 @@
+var computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s","d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+
+$(#"wins") = 0;
+$(#"loses") = 0;
+$(#"guessleft") = 10;
