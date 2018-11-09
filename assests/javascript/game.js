80f21e9fec3295a77e15f2a7e4b1fd998ba5115c
Bryan Liu <bryanliu@MacBook-Pro.local>
2018-11-09 09:34:21 -0800

    first draft

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,5 +1,5 @@
 var computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
-//var myChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s","d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+
 
 
 var winsScore = 0;
@@ -10,27 +10,27 @@ var letter = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "
 $('#wins').append(winsScore);
 $('#loses').append(losesScore);
 $('#guessleft').append(guessScore);
+ var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-//$('#guessleft').onkeyup
 document.onkeyup = function (event) {
 
     // Determines which key was pressed.
     var myChoices = event.key;
-
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+var letterGuessWrite = function () {
+            var div = $("<div>");
+            div.text(myChoices);
+            console.log(myChoices);
+            $("#lettersguessed").append(div);
+        }
+   
     console.log(computerGuess)
     
     if (myChoices === "a" || myChoices === "s" || myChoices === "d" || myChoices === "f" || myChoices === "g" || myChoices === "h" || myChoices === "j" || myChoices === "k" || myChoices === "l") {
 
        
 
-        var b = function () {
-            var a = $("<div>");
-            a.text(myChoices);
-            console.log(myChoices);
-            $("#lettersguessed").append(a);
-        }
-        b();
+        
+        letterGuessWrite();
 
         for (var i = 0; i >= 0; i--) {
             guessScore--;
@@ -38,10 +38,11 @@ document.onkeyup = function (event) {
             if (guessScore == 0) {
                 losesScore++;
                 $('#loses').html(losesScore);
+                 computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
             }
             if (guessScore === 0) {
                 guessScore = 10;
-                var a = $("<div>");
+                
 
             
 
@@ -50,6 +51,7 @@ document.onkeyup = function (event) {
             if (computerGuess === myChoices) {
                 winsScore++; $('#wins').html(winsScore);
                 guessScore = 10;
+                computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
             }
 
@@ -63,4 +65,3 @@ document.onkeyup = function (event) {
 
 
 
-//"q" || "w" || "e" || "r" || "t" || "y" || "u" || "i" || "o" || "p" || "a" || "s" || "d" || "f" || "g" || "h" || "j" || "k" || "l" || "z" || "x" || "c" || "v" || "b" || "n" || "m" 
