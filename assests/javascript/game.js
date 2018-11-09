b96ff463bad77dc9f41ff88344d23a6bd40ed52d
Bryan Liu <bryanliu@macbook-pro.attlocal.net>
2018-11-09 04:11:16 -0800

    score card added

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,5 +1,10 @@
 var computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s","d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 
-$(#"wins") = 0;
-$(#"loses") = 0;
-$(#"guessleft") = 10;
+
+var winsScore = 0;
+var losesScore = 0;
+var guessScore = 10;
+
+$('#wins').append(winsScore);
+$('#loses').append(losesScore);
+$('#guessleft').append(guessScore);
