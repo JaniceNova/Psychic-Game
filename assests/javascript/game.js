937a2ec3ff1128d80b5243f3b0f7aa9f853d4ae3
Bryan Liu <bryanliu@macbook-pro.attlocal.net>
2018-11-09 05:00:22 -0800

    got guess counter to count down

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,4 +1,5 @@
 var computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s","d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+//var myChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s","d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 
 
 var winsScore = 0;
@@ -8,3 +9,25 @@ var guessScore = 10;
 $('#wins').append(winsScore);
 $('#loses').append(losesScore);
 $('#guessleft').append(guessScore);
+
+//$('#guessleft').onkeyup
+document.onkeyup = function(event) {
+
+    // Determines which key was pressed.
+    var myChoices = event.key;
+
+    // Randomly chooses a choice from the options array. This is the Computer's guess.
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    // Only run the following code block if the user presses "r" or "p" or "s".
+    if (myChoices === "q" || "w" || "e" || "r" || "t" || "y" || "u" || "i" || "o" || "p" || "a" || "s" || "d" || "f" || "g" || "h" || "j" || "k" || "l" || "z" || "x" || "c" || "v" || "b" || "n" || "m" ) {
+            guessScore--;
+            $('#guessleft').html(guessScore);
+        }
+    if (computerChoices === myChoices) {
+        winsScore++;
+    }
+    if (guessScore === 0){
+        losesScore++;
+    }
+}
