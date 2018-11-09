b49dfa8be7d2d52e9c13ef84e8ba4b8a4be20187
Bryan Liu <bryanliu@macbook-pro.attlocal.net>
2018-11-09 06:13:38 -0800

    got choices to pop up

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,48 +1,58 @@
-var computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s","d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+var computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 //var myChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s","d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 
 
 var winsScore = 0;
 var losesScore = 0;
 var guessScore = 10;
+var letter = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 
 $('#wins').append(winsScore);
 $('#loses').append(losesScore);
 $('#guessleft').append(guessScore);
 
 //$('#guessleft').onkeyup
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 
     // Determines which key was pressed.
     var myChoices = event.key;
 
     // Randomly chooses a choice from the options array. This is the Computer's guess.
-      var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        console.log(computerGuess)
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log(computerGuess)
     // Only run the following code block if the user presses "r" or "p" or "s".
-    if (myChoices === "a") {    
-      
-          for (var i = 0; i>=0; i--){
+    if (myChoices === "a" || myChoices === "s" || myChoices === "d") {
+
+       // $('#lettersguessed').html(letter);
+     
+        var a = $("<div>");  
+          a.text(myChoices);
+          console.log(myChoices);
+         $("#lettersguessed").append(a);
+
+
+        for (var i = 0; i >= 0; i--) {
             guessScore--;
             $('#guessleft').html(guessScore);
-            if (guessScore == 0){
-            losesScore++;
-            $('#loses').html(losesScore);}
+            if (guessScore == 0) {
+                losesScore++;
+                $('#loses').html(losesScore);
+            }
             if (guessScore === 0) {
                 guessScore = 10;
             }
-             
-             if (computerGuess === myChoices) {
-                winsScore++;  $('#wins').html(winsScore);
+
+            if (computerGuess === myChoices) {
+                winsScore++; $('#wins').html(winsScore);
                 guessScore = 10;
 
-        }
-    
-        }
-         }
+            }
 
-         }
-    
+        }
+    }
+
+}
+
 
 
 
