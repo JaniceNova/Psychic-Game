ed832339883a9d021db7e41e08d9361650c37151
Bryan Liu <bryanliu@macbook-pro.attlocal.net>
2018-11-09 05:32:15 -0800

    guess counter resets at 1

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,6 +1,6 @@
 var computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s","d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 //var myChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s","d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
-
+var computerChoices = ["a"];
 
 var winsScore = 0;
 var losesScore = 0;
@@ -17,17 +17,32 @@ document.onkeyup = function(event) {
     var myChoices = event.key;
 
     // Randomly chooses a choice from the options array. This is the Computer's guess.
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+      var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
     // Only run the following code block if the user presses "r" or "p" or "s".
-    if (myChoices === "q" || "w" || "e" || "r" || "t" || "y" || "u" || "i" || "o" || "p" || "a" || "s" || "d" || "f" || "g" || "h" || "j" || "k" || "l" || "z" || "x" || "c" || "v" || "b" || "n" || "m" ) {
+    if (myChoices === "a") {    
+      
+          for (var i = 0; i>=0; i--){
             guessScore--;
             $('#guessleft').html(guessScore);
+            if (guessScore == 0){
+            losesScore++;
+            if (guessScore === 0) {
+                guessScore = 10;
+            }
+             $('#loses').html(losesScore);}
+             if (computerChoices === myChoices) {
+        winsScore++;  $('#wins').html(winsScore);
         }
-    if (computerChoices === myChoices) {
-        winsScore++;
-    }
-    if (guessScore === 0){
-        losesScore++;
-    }
-}
+    
+        }
+         }
+
+         }
+    
+
+
+
+
+
+//"q" || "w" || "e" || "r" || "t" || "y" || "u" || "i" || "o" || "p" || "a" || "s" || "d" || "f" || "g" || "h" || "j" || "k" || "l" || "z" || "x" || "c" || "v" || "b" || "n" || "m" 
